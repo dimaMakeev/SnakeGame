f1be64dde7bb513f7360775b2c9aeea2d7e241ea
Dmytro Makieiev <Dmytro.Makieiev@tabcorp.com.au>
2020-09-22 14:33:55 +1000

    adding settings file

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import math
 import random
-import pygame
-import pygame.display
 import tkinter as tk
 from tkinter import messagebox
+
+import pygame
+
+
 
 class cube(object):
     rows = 20
@@ -46,15 +48,18 @@ class snake(object):
                     self.dirnx = 0
                     self.dirny = 1
                     self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
-         for i,c in enumerate(self.body):
+        for i,c in enumerate(self.body):
             p=c.pos[:]
             if p in self.turns:
                  c.move(turn[0], turn[1])
                  if i == len(self.body)-1:
                      self.turns.pop(p) 
             else: 
-                if c.dirnx = -1 and c.pos[0] <= 0: c.pos = (c.rows-1, c.pos[1])
-
+                if c.dirnx == -1 and c.pos[0] <= 0: c.pos = (c.rows-1, c.pos[1])
+                elif c.dirnx == 1 and c.pos[0] >= c.rows-1: c.pos = (0,c.pos[1])
+                elif c.dirny == 1 and c.pos[1] >= c.rows-1: c.pos = (c.pos[0], 0)
+                elif c.dirny == -1 and c.pos[1] <= 0: c.pos = (c.pos[0],c.rows-1)
+                else: c.move(c.dirnx,c.dirny)
 
 
 def redrawWindow(surface):
