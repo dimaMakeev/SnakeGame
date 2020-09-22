8022f73de5b2369cb63f3357be00eecf28dfc4b1
Dmytro Makieiev <Dmytro.Makieiev@tabcorp.com.au>
2020-09-22 13:30:35 +1000

    adding keys handeling

--- main.py
+++ main.py
@@ -17,8 +17,46 @@ class cube(object):
 class snake(object):
     body = []
     turns = {}
-    def __init__(self):
-        pass 
+
+    def __init__(self, color, pos):
+        self.color = color
+        self.head = cube(pos) 
+        self.body.append(self.head)
+        self.dirnx = 0
+        self.dirny = 1
+    def move(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            keys = pygame.key.get_pressed()
+            for key in keys:
+                if keys[pygame.K_LEFT]:
+                    self.dirnx =-1
+                    self.dirny = 0
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+                elif keys[pygame.K_RIGHT]:
+                    self.dirnx =1
+                    self.dirny = 0
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+                elif keys[pygame.K_UP]:
+                    self.dirnx = 0
+                    self.dirny = -1
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+                elif keys[pygame.K_DOWN]:
+                    self.dirnx = 0
+                    self.dirny = 1
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+         for i,c in enumerate(self.body):
+            p=c.pos[:]
+            if p in self.turns:
+                 c.move(turn[0], turn[1])
+                 if i == len(self.body)-1:
+                     self.turns.pop(p) 
+            else: 
+                if c.dirnx = -1 and c.pos[0] <= 0: c.pos = (c.rows-1, c.pos[1])
+
+
+
 def redrawWindow(surface):
     global width, rows
     surface.fill((0,0,0))
@@ -28,7 +66,7 @@ def drawGrid(w, rows, surface):
     sizeBeetwen = w//rows
     x = 0
     y = 0
-    for l in range (rows):
+    for l in rows:
         x = x + sizeBeetwen
         y = y + sizeBeetwen
         pygame.draw.line(surface, (255, 255, 255), (x,0), (x,w))
@@ -42,7 +80,7 @@ def main():
     win = pygame.display.set_mode((width, width))
     #s = snake((255,0,0), (10,10))
     flag = True  
-    
+    #pygame.display.init()
     clock = pygame.time.Clock()
     
     while flag:
